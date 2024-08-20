efe0b3eb8ce1208dfb8ce980c3aa7edb55a7c378
jh.jun <cnova0713@vadaro.co.kr>
2024-08-20 10:50:22 +0900

    1_view 환경 설정-controller-temaplates 테스트

--- 1_spring_getting_started/hello-spring/src/main/java/hello/hello_spring/HelloSpringApplication.java
+++ 1_spring_getting_started/hello-spring/src/main/java/hello/hello_spring/HelloSpringApplication.java
@@ -3,7 +3,7 @@ package hello.hello_spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// 해당 어노테이션 내부 톰캣 서버 내장되어 있고 프로젝트 실행 되게 해준다.
+// 해당 어노테이션 톰캣 서버 내장된채로 프로젝트 실행 되게 해준다.
 @SpringBootApplication
 public class HelloSpringApplication {
 	public static void main(String[] args) {
