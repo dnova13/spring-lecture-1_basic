8bb93bd109f1575c3f6df67fbdf914f4db2bcdb4
jh.jun <cnova0713@vadaro.co.kr>
2024-08-27 05:39:39 +0900

    7 내용에 대한 주석 추가

--- 1_spring_getting_started/hello-spring/src/main/java/hello/hellospring/aop/TimeTraceAop.java
+++ 1_spring_getting_started/hello-spring/src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class TimeTraceAop {
     @Around("execution(* hello.hellospring..*(..))")
+//    @Around("execution(* com.example.yourpackage..*(..))") // yourpackage 부분은 실제 패키지 경로로 변경
     public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
         System.out.println("START: " + joinPoint.toString());
