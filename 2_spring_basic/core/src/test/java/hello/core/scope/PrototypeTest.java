8405a210cc0d994aad7d585836d7c03ca673be6b
jh.jun <cnova0713@vadaro.co.kr>
2024-09-14 06:09:32 +0900

    10. 빈스코프 10-1. 프로토 타입 스코프 10-2. 프로토 타입 스코프 - 싱글톤 빈과 함께 사용시 생기는 문제점

---
+++ 2_spring_basic/core/src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,56 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        System.out.println("find prototypeBean1");
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+
+        System.out.println("find prototypeBean2");
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+
+        Assertions.assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+
+        /*
+        *
+        * 싱글톤과 달리 프로토타입 빈은 스프링 컨테이너가 종료될 때 @PreDestroy 같은 종료 메서드가
+        * 전혀 실행되지 않는다.
+        *
+        * 그러므로 아래 처럼 수동으로 직접 호출해야한다.
+        *
+        * */
+        prototypeBean1.destroy();
+        prototypeBean2.destroy();
+
+        ac.close();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
