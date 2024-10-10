407520388dc7a0f3e44660ad36af43e3e390b4e9
jh.jun <dnova13@gmail.com>
2024-10-11 07:15:35 +0900

    32. 스프링 MVC 웹 페이지 만들기 - 상품 상세 & 상품 등록 폼

--- 3_1_spring_mvc_p1/3_item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ 3_1_spring_mvc_p1/3_item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.annotation.PostConstruct;
@@ -35,6 +37,24 @@ public class BasicItemController {
         model.addAttribute("items", items);
 
         return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String items(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String save() {
+        return "basic/addForm";
     }
 
     @PostConstruct
