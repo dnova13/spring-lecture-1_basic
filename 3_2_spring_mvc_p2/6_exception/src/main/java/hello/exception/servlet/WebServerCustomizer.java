72b6e0b2f8095bbd1c4b0a094bc1f0d1e71a875a
jh.jun <dnova13@gmail.com>
2024-10-18 04:58:24 +0900

    43. 예외 처리와 오류 페이지 - 스프링 부트 오류 페이지

--- 3_2_spring_mvc_p2/6_exception/src/main/java/hello/exception/servlet/WebServerCustomizer.java
+++ 3_2_spring_mvc_p2/6_exception/src/main/java/hello/exception/servlet/WebServerCustomizer.java
@@ -6,7 +6,14 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
+/*
+* @Component를 주석 처리하자.
+*
+* 이제 오류가 발생했을 때 오류 페이지로 /error를 기본 요청한다.
+* 스프링 부트가 자동 등록한 BasicErrorController는 이 경로를 기본으로 받는다.
+* 
+* */
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
 
     @Override
